d29a2baa238d078a4b4dbd2a490e1a5369c0f806
pellenberger <p.ellenberger@outlook.com>
2015-10-11 20:52:11 +0200

    Add StorageService methods

--- app/scripts/services/storageservice.js
+++ app/scripts/services/storageservice.js
@@ -8,13 +8,28 @@
  * Service in the billetApp.
  */
 angular.module('billetApp')
-  .service('StorageService', function () {
-	  
+  .service('StorageService', function (localStorageService) {
+
+    var CURRENT_LIST = 'currentList';
+    var LISTS = 'lists';
+
 	  return {
-		  
-		 test : function() {
-			 console.log("service test");
-		 } 
-		  
-	  }  
+		 getLists : function() {
+       return localStorageService.get(LISTS);
+		 },
+     saveList : function(id, name) {
+       var lists = this.getLists();
+       if (!lists) {
+         lists = [];
+       }
+       lists.push({'id' : id, 'name' : name});
+       localStorageService.set(LISTS, lists);
+     },
+     getCurrentList : function() {
+       return localStorageService.get(CURRENT_LIST);
+     },
+     setCurrentList : function(id, name) {
+       localStorageService.set(CURRENT_LIST, {'id' : id, 'name' : name});
+     }
+	  }
   });
