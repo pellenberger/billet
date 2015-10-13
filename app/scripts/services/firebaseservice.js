d65312d33bb755444985d166f888d96a54048608
pellenberger <p.ellenberger@outlook.com>
2015-10-13 22:47:54 +0200

    Add get lists and create list methods on firebaseservice

--- app/scripts/services/firebaseservice.js
+++ app/scripts/services/firebaseservice.js
@@ -5,33 +5,34 @@
  * @name billetApp.FirebaseService
  * @description # FirebaseService Service in the billetApp.
  */
-angular.module('billetApp').service('FirebaseService', function() {
-	
-	var ref = new Firebase("https://billet.firebaseio.com/lists");	
+angular.module('billetApp').service('FirebaseService', function($firebaseArray) {
+
+	var listsRef = new Firebase("https://billet.firebaseio.com/lists");
 	var authToken = "Z5FWNA4t2jzM0Nq1R8RuknZbay3nheavChIhxlwU";
 
+  connect();
+
+  var lists = $firebaseArray(listsRef);
+
+  function connect() {
+    listsRef.authWithCustomToken(authToken, function(error) {
+      if (error) {
+        // TODO error handling
+        console.log("Login Failed!", error);
+      }
+    });
+  }
+
 	return {
-		connect: function(){			
-			ref.authWithCustomToken(authToken, function(error, authData) {
-			  if (error) {
-			    console.log("Login Failed!", error);
-			  } else {
-			    console.log("Login Succeeded!", authData);
-			  }
-			});			
-		},
 		createList: function(name) {
-			ref.push({
-				'name' : name
-			}, function(error){
-				if (error) {
-					console.log("Create list failed : " + error);
-				}
-				else {
-					console.log("Create list succeeded");
-				}
-			});
-		}
+      return lists.$add({
+        'name' : name,
+        'timestamp' : Firebase.ServerValue.TIMESTAMP
+      });
+		},
+    getLists: function() {
+      return lists;
+    }
 	};
 
 });
